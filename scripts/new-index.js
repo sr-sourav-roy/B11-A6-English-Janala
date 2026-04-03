33952018c75816c9e69a3ce1d1404f83116c29b1
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-04-03 14:28:11 +0600

    add by word details sections done

--- scripts/new-index.js
+++ scripts/new-index.js
@@ -18,6 +18,13 @@ document
     }
   });
 
+// removeActiveClass functions:
+function removeActiveClass() {
+  const activeBtn = document.getElementsByClassName("active");
+  for (let btn of activeBtn) {
+    btn.classList.remove("active");
+  }
+}
 // 🔃 All Levels load functions:
 function allLevelsLoad() {
   fetch("https://openapi.programming-hero.com/api/levels/all")
@@ -30,14 +37,28 @@ function allWordsLoad() {
     .then((res) => res.json())
     .then((data) => allWordsDisplay(data.data));
 }
-// 🔃Words by Levels:
+// 🔃Words by Levels load:
 const wordByLevelLoad = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => allWordsDisplay(data.data));
+    .then((data) => {
+      removeActiveClass();
+      const clickButton = document.getElementById(`btn-${id}`);
+      clickButton.classList.add("active");
+      // console.log(clickButton);
+      allWordsDisplay(data.data);
+    });
 };
+// 🔃load  Words Detail:
+function loadWordsDetail(id) {
+  const url2 = `https://openapi.programming-hero.com/api/word/${id}`;
+  console.log(url2);
+  fetch(url2)
+    .then((res) => res.json())
+    .then((data) => allWordDetails(data.data));
+}
 
 // allLevelsLoadDisplay
 // "id": 101,
@@ -48,14 +69,13 @@ function allLevelsLoadDisplay(allLevels) {
   for (const levels of allLevels) {
     const createDiv = document.createElement("div");
     createDiv.innerHTML = `
-         <button onclick="wordByLevelLoad(${levels.level_no})" class="btn hover:bg-sky-300">
+         <button id="btn-${levels.level_no}" onclick="wordByLevelLoad(${levels.level_no})" class="btn hover:bg-sky-300">
             <img src="./assets/fa-book-open.png" alt="" />${levels.lessonName}
           </button>
     `;
     btnLevels.append(createDiv);
   }
 }
-
 // allWordsDisplay:
 // "data": [
 // {
@@ -68,6 +88,20 @@ function allLevelsLoadDisplay(allLevels) {
 const allWordsDisplay = (allWords) => {
   const wordLevelContainer = document.getElementById("wordLevel-container");
   wordLevelContainer.innerHTML = "";
+
+  // condition নেক্সট Lesson এ যান:
+  if (allWords.length == 0) {
+    wordLevelContainer.innerHTML = `
+        <div class="col-span-full flex flex-col justify-center items-center">
+          <img class="w-[150px]" src="./assets/alert-error.png" alt="" />
+          <p class="text-[#79716B] text-sm mb-3">
+            এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+          </p>
+          <h1 class="text-2xl font-semibold">নেক্সট Lesson এ যান</h1>
+        </div>
+    `;
+    return;
+  }
   allWords.forEach((words) => {
     const createdDiv = document.createElement("div");
     createdDiv.innerHTML = `
@@ -80,7 +114,7 @@ const allWordsDisplay = (allWords) => {
       <div>
         <div class="flex justify-between">
           <button class="btn">
-            <img
+            <img onclick="loadWordsDetail(${words.id})"
               class="w-4 h-4"
               src="https://img.icons8.com/?size=48&id=18693&format=png"
               alt=""
@@ -99,6 +133,20 @@ const allWordsDisplay = (allWords) => {
     `;
     wordLevelContainer.append(createdDiv);
   });
+};
+// allWordsDetails:
+const allWordDetails = (data) => {
+  document.getElementById("word_Detail").showModal();
+  const wordDetailContainer = document.getElementById("word_Detail_container");
+  console.log(wordDetailContainer);
+  wordDetailContainer.innerHTML = `
+  <div class="card  bg-base-100 card-md shadow-sm">
+  <div class="card-body">
+    <h2 class="card-title">Medium Card</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+  </div>
+</div>
+  `;
 };
 allLevelsLoad();
 // allWordsLoad();
